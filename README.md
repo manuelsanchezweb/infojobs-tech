3953da1b22cf2a86edfa67a45f1e987624607201
manuelsanchez2 <manusansan22@gmail.com>
2023-05-18 11:26:06 +0200

    🆕 feat: Initial commit with client creation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Infojobs Tech
+
+Una nueva forma de descubrir infojobs para desarrolladores.
+
+## 📧 Contacto
+
+- [Xavier López](https://github.com/xavierlopez)
+- [Manuel Sánchez](https://github.com/manuelsanchezweb)
---
+++ client/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Infojobs Tech</h1>
+      <p>Coming soon</p>
+    </main>
+  )
+}
