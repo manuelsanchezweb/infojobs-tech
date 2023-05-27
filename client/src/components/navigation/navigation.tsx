155e13fc342eed178bccce6a94f55817244c4778
manuelsanchez2 <manusansan22@gmail.com>
2023-05-27 22:58:42 +0200

    🆕 feat: Fix navigation

--- client/src/components/navigation/navigation.tsx
+++ client/src/components/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { IconClose, IconMenu } from '../icons/Icons'
 import Link from 'next/link'
 import { Logo } from '../logo'
@@ -8,28 +8,17 @@ import { usePathname } from 'next/navigation'
 const Navigation = () => {
   const pathname = usePathname()
   const [isNavOpen, setIsNavOpen] = useState(false)
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen)
   }
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth)
-    }
-    window.addEventListener('resize', handleResize)
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
 
   return (
     <>
       <HeroLogo />
       <nav>
         {/* Desktop Menu  */}
-        {isNavOpen || windowWidth > 768 ? (
+        {isNavOpen && (
           <ul className="flex flex-col justify-center items-center gap-8 absolute top-0 left-0 bg-primaryLight h-screen w-full z-10 md:flex-row md:static md:bg-transparent md:h-full">
             <li>
               <Link
@@ -60,31 +49,63 @@ const Navigation = () => {
               </a>
             </li>
           </ul>
-        ) : (
-          ''
         )}
+        <ul className="hidden md:flex md:flex-row items-center gap-8 md:static md:bg-transparent md:h-full">
+          <li>
+            <Link
+              href="/"
+              className={`text-md md:text-sm ${
+                pathname === '/' ? 'font-bold' : ''
+              }`}
+            >
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/sobre-nosotros"
+              className={`text-md md:text-sm ${
+                pathname === '/sobre-nosotros' ? 'font-bold' : ''
+              }`}
+            >
+              Sobre nosotros
+            </Link>
+          </li>
+          <li>
+            <a
+              className="btn text-xs items-center"
+              href="https://github.com/manuelsanchezweb/infojobs-tech"
+            >
+              <IconGithub customClass="w-4" /> Ver repo
+            </a>
+          </li>
+        </ul>
 
-        {!isNavOpen && (
+        {!isNavOpen ? (
           <button
             key="open-menu"
             className="nav-open md:hidden"
-            aria-label="Open Menu"
+            aria-label="Abrir menú"
             onClick={toggleNav}
           >
             <IconMenu />
           </button>
+        ) : (
+          ''
         )}
 
         {/* Close Button  */}
-        {isNavOpen && (
+        {isNavOpen ? (
           <button
             key="close-menu"
             className="nav-close absolute right-4 top-8 z-20 md:hidden"
-            aria-label="Close Menu"
+            aria-label="Cerrar menú"
             onClick={toggleNav}
           >
             <IconClose />
           </button>
+        ) : (
+          ''
         )}
       </nav>
     </>
